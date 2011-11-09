b7c94da1e2c5b3c740e20120e87ab5306d1148ba
Nick Hardeman <nickhardeman@gmail.com>
2011-11-09 10:05:58 -0500

    changed arg to a pointer to the var being tweened

--- src/TweenEvent.h
+++ src/TweenEvent.h
@@ -24,17 +24,18 @@ private:
 	class Base {
 	public:
 		virtual ~Base() { }
-		virtual void operator()(float args) = 0;
+		virtual void operator()(float* args) = 0;
 	};
 	Base* pointerino;
 	bool _assigned;
 	
 public: 
-	
+	unsigned int ID; // this is the tween that is using it //
 	template <typename Class>
 	class T : public Base {
 		// Signature applied to a pointer-to-member for target class.
-		typedef void (Class::*Func)(float args);
+		typedef void (Class::*Func)(float* args);
+		
 		
 	private:
 		Class* mThis; // Pointer to the object we are delegating to.
@@ -42,16 +43,25 @@ public:
 		
 	public:
 		
-		T(Class* aThis, void (Class::*aFunc)(float args)) : mThis(aThis), mFunc(aFunc) { }
+		T(Class* aThis, void (Class::*aFunc)(float* args)) : mThis(aThis), mFunc(aFunc) { }
 		
-		virtual void operator()(float args) {
+		virtual void operator()(float* args) {
 			return (mThis->*mFunc)(args);
 		}
+		
+		
 	};
 	
-	~TweenEvent() {	}
+	~TweenEvent() {	}//cout << "calling the deconstructor" << endl;}//destroy(); }
 	
 	TweenEvent() {_assigned = false;}
+	// associates with the tween //
+	virtual void setID(unsigned int a_id) {
+		ID = a_id;
+	}
+	virtual unsigned int getID() {
+		return ID;
+	}
 	
 	void destroy() {
 		if (_assigned) {
@@ -64,7 +74,6 @@ public:
 	}
 	
 	
-	
 	TweenEvent& operator = (Base* aPtr) {
 		pointerino = aPtr;
 		_assigned = true;
@@ -75,9 +84,9 @@ public:
 		return _assigned;
 	}
 	
-	void operator()(float args) {
+	void operator()(float* args) {
 		if (pointerino) {
-			//cout << "TweenEvent :: operator args = " << args << "  _assigned = " << _assigned << endl;
+			//cout << "TweenEvent :: operator args = " << *args << "  _assigned = " << _assigned << endl;
 			(*pointerino)(args);
 		}
 	}
